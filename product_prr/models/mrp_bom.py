a1b837f1a71786ec41816b76fffc5b5ad2636e28
Ivan Candelas <ivan.candelas@pok.com.mx>
2022-02-01 16:36:22 -0600

    Removed field name

--- product_prr/models/mrp_bom.py
+++ product_prr/models/mrp_bom.py
@@ -4,17 +4,11 @@ from odoo import fields, models, api
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
 
-    name = fields.Char(
-        string='Name'
-    )
     default = fields.Boolean(
         string='Default',
         default=False
     )
-    default_name = fields.Char(
-        string='Name',
-        compute='_compute_get_default_name'
-    )
+
     time_cycle_manual_total = fields.Float(
         'Total ',
         compute="_compute_get_total_time_cycle_manual"
@@ -26,10 +20,3 @@ class MrpBom(models.Model):
             for operation in mrp.operation_ids:
                 mrp.time_cycle_manual_total += operation.time_cycle_manual
 
-    @api.depends('name')
-    def _compute_get_default_name(self):
-        for mrp_bom in self:
-            if mrp_bom.default is True:
-                mrp_bom.default_name = "Default"
-            else:
-                mrp_bom.default_name = ""
